2be863d1212ecc1a22924dec007f5c5292334f95
Svetlana Vassileva <72747194+svetlilaloli@users.noreply.github.com>
2021-03-24 07:27:16 +0000

    Add files via upload

---
+++ Follow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace TheVLogger
+{
+    class Follow
+    {
+        public HashSet<string> followers { get; set; }
+        public HashSet<string> following { get; set; }
+
+        public Follow()
+        {
+            followers = new HashSet<string>();
+            following = new HashSet<string>();
+        }
+    }
+}
---
+++ TheVLogger.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TheVLogger
+{
+    class TheVLogger
+    {
+        static void Main()
+        {
+            Dictionary<string, Follow> vloggers = new Dictionary<string, Follow>();
+            string command = Console.ReadLine();
+
+            while (command != "Statistics")
+            {
+                string[] currentCommand = command.Split();
+                string vloggerName = currentCommand[0];
+
+                if (currentCommand.Contains("joined"))
+                {
+                    if (!vloggers.ContainsKey(vloggerName))
+                    {
+                        vloggers.Add(vloggerName, new Follow());
+                    }
+                }
+                else if (currentCommand.Contains("followed"))
+                {
+                    string vlogger2 = currentCommand[2];
+                    if (vloggers.ContainsKey(vloggerName) && vloggers.ContainsKey(vlogger2) && vloggerName != vlogger2)
+                    {
+                        vloggers[vloggerName].following.Add(vlogger2);
+                        vloggers[vlogger2].followers.Add(vloggerName);
+                    }
+                }
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"The V-Logger has a total of {vloggers.Count} vloggers in its logs.");
+            int count = 1;
+            foreach (var vlogger in vloggers.OrderByDescending(x => x.Value.followers.Count).ThenBy(x => x.Value.following.Count))
+            {
+                Console.WriteLine($"{count}. {vlogger.Key} : {vlogger.Value.followers.Count} followers, {vlogger.Value.following.Count} following");
+                if (count == 1)
+                {
+                    foreach (var follower in vlogger.Value.followers.OrderBy(n => n))
+                    {
+                        Console.WriteLine($"*  {follower}");
+                    }
+                }
+                count++;
+            }
+        }
+    }
+}
